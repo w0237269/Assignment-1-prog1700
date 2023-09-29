7ae4b15512e902eef23c0e6a3be3b57d07f6de40
Matthew Miller <w0237269@nscc.ca>
2023-09-29 09:16:50 -0300

    pseudocode added to q3

--- q2_weeklyloan.js
+++ q2_weeklyloan.js
@@ -4,17 +4,23 @@ let readlineSync = require('readline-sync');
 // DO NOT EDIT: The main function to house our program code
 function main() 
 {
-    // Input
+    // Input get user to input loan amount, intrest rate, and number of years borrowing.
+
     let loan = parseFloat(readlineSync.question("Enter loan amount: "));
     let interestRate = parseFloat(readlineSync.question("Enter interest rate (annual): "));
     let years = parseFloat(readlineSync.question("Enter number of years: "));
 
     // Process
-    let weeklyInterestRate = (interestRate / 100) / 52; // Calculate weekly interest rate as a decimal
-    let numPayments = years * 52; // Total number of weekly payments
+    /* get weekly interest rate by dividing interest rate by 100 and then that by 52
+    get number of weekly payments by multiplying number of years by 52
+    get weekly payment amount by multiplying loan amount with weekly interest rate and follwing the rest of calculations I don't fully understand yet.
+    */
+
+    let weeklyInterestRate = (interestRate / 100) / 52; 
+    let numPayments = years * 52; 
     let weeklyPayment = (loan * weeklyInterestRate) / (1 - Math.pow(1 + weeklyInterestRate, -numPayments));
 
-    // Output
+    // Output the weekly payment amount with console log messaged plus weekly payment variable combined with the to fixed function.
     console.log("Your weekly payment will be: $" + weeklyPayment.toFixed(2));
 }
     // DO NOT EDIT: Trigger our main function to launch the program
